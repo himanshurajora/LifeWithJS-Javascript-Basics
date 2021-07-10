dd2b004d3895aa2bad056ff9ee0b5986e2bc3f59
Himanshu Jangid <iamahalofan@gmail.com>
2021-07-10 21:48:01 +0530

    Log and Variables

---
+++ HelloWorld.js
@@ -0,0 +1,36 @@
+// Single Line Comment
+/*
+    Multiline
+    Comment
+*/
+
+// The Hello World Program!!
+// printf("Hello World! in C");
+// print("Hello World in Python");
+
+console.log("Hello World!");
+
+// Variables 
+
+// var name = something;
+
+// Number 
+var num = 10;
+num = 10.1;
+num = 40.134;
+
+console.log(num);
+
+// String
+var str = "JavaScript";
+
+console.log("JavaScript");
+
+// Printing Multiple Values
+
+console.log("hello", 2000);
+
+// also multiple variables
+
+console.log(str, num);
+
