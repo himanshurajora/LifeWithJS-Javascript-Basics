2360a37c8d4e2048a4522fe5526d256d0bb0f42e
himanshurajora <iamahalofan@gmail.com>
2022-05-18 23:28:49 +0530

    console logs

---
+++ Basics Concepts/consoleLogs.js
@@ -0,0 +1,12 @@
+// simple hello world
+console.log("Hello World")
+
+// two strings together
+console.log("My name is", "John")
+
+// multiple strings and numbers
+console.log("My name is", "John", "and I am", 30, "years old")
+
+// evaluating expressions in the console log
+console.log("The Addition of 10 and 20 is", 10 + 20)
+
