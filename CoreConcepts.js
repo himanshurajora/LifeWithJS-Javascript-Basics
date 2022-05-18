445e643e4abe0c4becbb3bf26d31108bd004fbc5
himanshurajora <iamahalofan@gmail.com>
2022-05-18 22:50:54 +0530

    core concepts of javascript

---
+++ CoreConcepts.js
@@ -0,0 +1,12 @@
+// The Core concepts of Javascript
+1. Hello World
+2. Variables
+3. Data Types
+4. Conditional Statements
+5. Loops
+6. Functions
+7. Arrays
+8. Objects
+9. Object Oriented Programming
+10. Classes
+11. Inheritance
